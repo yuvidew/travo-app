537aa261ff2cfb4376276aa6173bf0082e9f727e
Yuvraj dewangan <yd00102@gmail.com>
2025-08-24 12:08:42 +0530

    feat: creating a welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,10 @@ import "./global.css"
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{headerShown : false}} >
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="(root)" options={{ headerShown: false }} /> */}
+      </Stack>
+  );
 }
---
+++ constants/images.ts
@@ -0,0 +1,5 @@
+// constants/images.ts
+export const images = {
+  welcome: require('../assets/images/welcome.png'), // adjust path/extension
+  // ...other images
+};
